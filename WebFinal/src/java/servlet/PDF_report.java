470f7c67b1a7c8cc61f43542bd539ff747524bcb
FMalesani <FMalesani@192.168.1.131>
2013-11-20 11:08:24 +0100

    pdf7

--- WebFinal/src/java/servlet/PDF_report.java
+++ WebFinal/src/java/servlet/PDF_report.java
@@ -117,7 +117,6 @@ public class PDF_report extends HttpServlet {
             
             //DATO L'UTENTE E IL GRUPPO DEVO RECUPERARE DAL DATABASE IL NUMERO DI POST DELL'UTENTE NEL GRUPPO
             //DATO L'UTENTE E IL GRUPPO DEVO RECUPERARE DAL DATABASE LA DATA DELL'ULTIMO POST DELL'UTENTE NEL GRUPPO
-            //DEVO RECUPERARE DAL DATABASE LA PATH DELL'AVATAR DELL'UTENTE FATOOOOO!!
             
             Image myAvatar = Image.getInstance("/Users/FMalesani/NetBeansProjects/Steeeee/web/images/user_icon.jpg");
             myAvatar.scaleToFit(50,50);
