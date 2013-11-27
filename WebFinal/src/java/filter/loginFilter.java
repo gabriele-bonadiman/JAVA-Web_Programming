316e915b90e4ae055a3a9d263e3229e2efbfbd57
FMalesani <FMalesani@10.23.103.86>
2013-11-27 11:54:26 +0100

    login filter

---
+++ WebFinal/src/java/filter/loginFilter.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package filter;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author FMalesani
+ */
+
+public class loginFilter implements Filter {
+    
+    private static final boolean debug = false;
+
+    // The filter configuration object we are associated with.  If
+    // this value is null, this filter instance is not currently
+    // configured. 
+    private FilterConfig filterConfig = null;
+    
+    public loginFilter() {
+    }    
+
+    /**
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        
+        Object bean = ((HttpServletRequest)request).getSession().getAttribute("utente");
+        
+        if (bean == null){
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            out.println("<head>");
+            out.println("<meta http-equiv=\"refresh\" content=\"2; Login\">");
+            out.println("<div align=’center’><H3>IMPOSSIBILE ACCEDERE: NON SEI LOGGATO<br><br>");
+            out.println("Redirezione alla pagina di principale in corso</H3></div>");
+            out.close();
+            
+        } else 
+            chain.doFilter(request, response);
+    }
+
+    /**
+     * Destroy method for this filter
+     */
+    public void destroy() {  
+        this.filterConfig = null;
+    }
+
+    /**
+     * Init method for this filter
+     */
+    public void init(FilterConfig filterConfig) {        
+        this.filterConfig = filterConfig;
+    }
+}
